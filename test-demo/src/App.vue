af8d58039de586f853a4154cc91e940d15472583
CWH0908 <2515145957@qq.com>
2021-12-14 20:32:21 +0800

    fix: 添加测试组件

--- test-demo/src/App.vue
+++ test-demo/src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png" />
+    <!-- <Test /> -->
     <CwhButton />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 import { Button as CwhButton } from 'cwh-ui'
+import Test from './components/test.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Test,
     CwhButton
   }
 }

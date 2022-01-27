3a9226c3aebdb30cfc2f440ee4de8c65ec90d106
CWH0908 <2515145957@qq.com>
2022-01-27 18:09:34 +0800

    fix: todoList升级到vue3

--- ToDoList/src/App.vue
+++ ToDoList/src/App.vue
@@ -1,12 +1,10 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
@@ -15,14 +13,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
